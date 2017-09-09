8a57c10dbd9439a3c7098cc3af97e8307016c6e4
Svend <svend.jin@qq.com>
2017-09-09 17:00:09 +0800

    add 91pron support

--- video18XXOO.user.js
+++ video18XXOO.user.js
@@ -1,21 +1,47 @@
 // ==UserScript==
 // @name         视频网站破解
 // @namespace    undefined
-// @version      0.0.2
-// @description  :underage: 视频网站破解
+// @version      0.0.3
+// @description  :underage: 视频网站破解(http://ripe.cf/)
 // @author       16Free
 // @match        http://www.ri003.com/videos/*
+// @match        http://91.91p17.space/view_video.php?*
 // @require      https://code.jquery.com/jquery-latest.js
 // @run-at       document-start
 // @grant        unsafeWindow
 // @grant        GM_setClipboard
 // ==/UserScript==
 $(document).ready(function () {
-    setCookie('video_log', 0, '.ri003.com', '/');
+    var href = window.location.href;
 
-    createVideoEle();
+    if(href.indexOf('91.91p17.space')){
+        pron91();
+    }else if(href.indexOf('www.ri003.com')){
+        ri003();
+    }
+
 });
 
+
+function pron91() {
+    var url = $('#vid_html5_api').find('source')[0].src;
+    createVideoEle(url);
+}
+
+
+function ri003() {
+    setCookie('video_log', 0, '.ri003.com', '/');
+
+    if (flashvars) {
+        var videoUrl = flashvars.video_url;
+        if (videoUrl[videoUrl.length - 1] === '/')
+            videoUrl = videoUrl.substr(0, videoUrl.length - 1);
+
+        createVideoEle(videoUrl);
+    }
+
+
+}
 
 function setCookie(name, value, domain, path) {
     var Days = 30;
@@ -27,33 +53,27 @@ function setCookie(name, value, domain, path) {
     console.log('set cookie:', cookie);
 }
 
-function createVideoEle() {
-    if (flashvars) {
-        var videoUrl = flashvars.video_url;
-        if (videoUrl[videoUrl.length - 1] === '/')
-            videoUrl = videoUrl.substr(0, videoUrl.length - 1);
+function createVideoEle(url) {
+    console.log('create video ele, url is :', videoUrl);
 
-        console.log('create video ele, url is :', videoUrl);
-
-        var videoDiv = '<div id="user_video_div"><video id="user_video" controls="controls" autoplay></video></div>';
-        $(document.body).empty();
-        $(document.body).css({
-            width: '100%',
-            height: '100%'
-        });
-        $(document.body).append(videoDiv);
-        $('#user_video').attr('src', videoUrl);
-        $('#user_video_div').css({
-            width: '100%',
-            height: '100%',
-            position: 'absolute',
-            top: '0',
-            left: '0',
-            background:'#fff'
-        });
-        $('#user_video').css({
-            width: '100%',
-            height: '100%'
-        });
-    }
+    var videoDiv = '<div id="user_video_div"><video id="user_video" controls="controls" autoplay></video></div>';
+    $(document.body).empty();
+    $(document.body).css({
+        width: '100%',
+        height: '100%'
+    });
+    $(document.body).append(videoDiv);
+    $('#user_video').attr('src', videoUrl);
+    $('#user_video_div').css({
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+        top: '0',
+        left: '0',
+        background: '#fff'
+    });
+    $('#user_video').css({
+        width: '100%',
+        height: '100%'
+    });
 }
