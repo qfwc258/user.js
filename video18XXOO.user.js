1337a11933011a13af96247050d84cb079b28559
Svend <svend.jin@qq.com>
2017-09-10 14:28:20 +0800

    remove ad

--- video18XXOO.user.js
+++ video18XXOO.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         视频网站破解
 // @namespace    undefined
-// @version      0.0.6
+// @version      0.0.2
 // @description  :underage: 视频网站破解(http://ripe.cf/)
 // @author       16Free
-// @match        http://www.ri003.com/videos/*
+// @match        http://www.ri003.com/*
 // @require      https://code.jquery.com/jquery-latest.js
 // @run-at       document-start
 // @grant        unsafeWindow
@@ -17,12 +17,18 @@ $(document).ready(function () {
 function ri003() {
     setCookie('video_log', 0, '.ri003.com', '/');
 
+    //ad
+    $('div[align="center"]').remove();
+    $('div.footer-margin').remove();
+    $('noindex').remove();
+
     if (flashvars) {
         var videoUrl = flashvars.video_url;
         if (videoUrl[videoUrl.length - 1] === '/')
             videoUrl = videoUrl.substr(0, videoUrl.length - 1);
 
-        createVideoEle(videoUrl);
+        console.log('play video url :', videoUrl);
+        createVideoEle('#kt_player',videoUrl);
     }
 
 
@@ -38,21 +44,13 @@ function setCookie(name, value, domain, path) {
     console.log('set cookie:', cookie);
 }
 
-function createVideoEle(url) {
-    console.log('create video ele, url is :', url);
-
+function createVideoEle(ele,url) {
     var videoDiv = '<div id="user_video_div"><video id="user_video" controls="controls" autoplay></video></div>';
-    $(document.body).empty();
-    $(document.body).css({
-        width: '100%',
-        height: '100%'
-    });
-    $(document.body).append(videoDiv);
+    $(ele).html(videoDiv);
     $('#user_video').attr('src', url);
     $('#user_video_div').css({
         width: '100%',
         height: '100%',
-        position: 'absolute',
         top: '0',
         left: '0',
         background: '#fff'
